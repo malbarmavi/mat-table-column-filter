422bae857349e9a2c0dc3bf94f39edc505f16c06
Muhammed Albarmavi <muhammed.albarmavi@outlook.com>
2021-02-01 17:14:18 +0300

    generate build code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  buildCode: "####",
 };
+
+console.log(environment.buildCode);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  buildCode: "####",
 };
+
+console.log(environment.buildCode);
 
 /*
  * In development mode, to ignore zone related error stack frames such as
