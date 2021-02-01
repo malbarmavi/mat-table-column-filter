c133b4c973f5e16149b1bb79f08bc12ddff6bbc4
Muhammed Albarmavi <muhammed.albarmavi@outlook.com>
2021-02-01 18:58:25 +0300

    fix pre build file path problem

--- pre-build.js
+++ pre-build.js
@@ -1,37 +1,40 @@
 const { execSync } = require("child_process");
 const fs = require("fs");
 const path = require("path");
+try {
+  const basePath = path.join(__dirname, "src", "environments");
+  const files = ["prod"].map((ex) =>
+    path.join(basePath, `environment.${ex}.ts`)
+  );
+  console.log(files);
+  const { TRAVIS_BUILD_NUMBER } = process.env;
+  console.log(TRAVIS_BUILD_NUMBER);
+  const gitCommand = "git log -1";
+  const gitResult = execSync(gitCommand)
+    .toString()
+    .split(`\n`)
+    .map((r) => r.trim())
+    .filter((r) => r);
 
-const basePath = path.join(__dirname, `\\src\\environments`);
-const files = ["prod"].map((ex) =>
-  path.join(basePath, `\\environment.${ex}.ts`)
-);
-console.log(files);
-const { TRAVIS_BUILD_NUMBER } = process.env;
-console.log(TRAVIS_BUILD_NUMBER);
-const gitCommand = "git log -1";
-const gitResult = execSync(gitCommand)
-  .toString()
-  .split(`\n`)
-  .map((r) => r.trim())
-  .filter((r) => r);
+  // const branch = execSync("git branch --show-current").toString().trim();
+  const branch = execSync("git branch")
+    .toString()
+    .trim()
+    .split("\n")[0]
+    .substr(2);
 
-// const branch = execSync("git branch --show-current").toString().trim();
-const branch = execSync("git branch")
-  .toString()
-  .trim()
-  .split("\n")[0]
-  .substr(2);
+  let [commit] = gitResult;
+  commit = commit.split(" ")[1];
+  console.log(commit);
 
-let [commit] = gitResult;
-commit = commit.split(" ")[1];
-console.log(commit);
+  const buildMessage = `${branch}-${commit}-${TRAVIS_BUILD_NUMBER}`;
 
-const buildMessage = `${branch}-${commit}-${TRAVIS_BUILD_NUMBER}`;
-
-files.forEach((filePath) => {
-  const f = fs.readFileSync(filePath).toString();
-  const result = f.replace("####", buildMessage);
-  fs.writeFileSync(filePath, result);
-  console.log(result);
-});
+  files.forEach((filePath) => {
+    const f = fs.readFileSync(filePath).toString();
+    const result = f.replace("####", buildMessage);
+    fs.writeFileSync(filePath, result);
+    console.log(result);
+  });
+} catch (err) {
+  console.log("error: ", err);
+}
